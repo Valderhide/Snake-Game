c3bc7ab5caf03f2c6c85e5334b149a8d8d801967
Valderhide <gpinsent@hotmail.com>
2021-08-05 22:58:46 -0230

    First grid cell

---
+++ Snake-Game.js
@@ -0,0 +1,16 @@
+initialize();
+
+function initialize(){
+    var div = document.createElement('div');
+    div.className = 'pixel';
+    var gameboardDiv = document.getElementById("gameboard");
+    gameboardDiv.appendChild(div);
+
+    for(){//rows
+        for(){//for each row, go through each dive
+
+        }
+
+    }
+
+}
