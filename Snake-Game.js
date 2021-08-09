3a18151653b8efa2a1ed8f52033b8142aa957267
Valderhide <gpinsent@hotmail.com>
2021-08-09 17:41:52 -0230

    Attempt 1: Edit CSS with JS

--- Snake-Game.js
+++ Snake-Game.js
@@ -1,16 +1,13 @@
 initialize();
 
+
 function initialize(){
     var div = document.createElement('div');
     div.className = 'pixel';
+    
     var gameboardDiv = document.getElementById("gameboard");
     gameboardDiv.appendChild(div);
-
-    for(){//rows
-        for(){//for each row, go through each dive
-
-        }
-
-    }
+    
+    document.getElementsByClassName('pixel').style.left = 300
 
 }
