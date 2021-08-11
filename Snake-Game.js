965dc2a0922fa9cace976ed7b5c4425c0a6ba9c4
Valderhide <gpinsent@hotmail.com>
2021-08-11 15:48:57 -0230

    For loop & position fix

--- Snake-Game.js
+++ Snake-Game.js
@@ -1,13 +1,17 @@
 initialize();
 
 
+function delay() {
+    setTimeout(initialize, 3000);
+  }
+
 function initialize(){
     var div = document.createElement('div');
     div.className = 'pixel';
     
     var gameboardDiv = document.getElementById("gameboard");
     gameboardDiv.appendChild(div);
-    
-    document.getElementsByClassName('pixel').style.left = 300
 
+     for (let r = 0; r<150 ; r++)
+        div.style.left = r;
 }
