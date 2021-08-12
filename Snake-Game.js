dd727d1ad2505a8a0152210219a9699f5f8c76a2
Valderhide <gpinsent@hotmail.com>
2021-08-11 22:36:50 -0230

    GameBoard + Snake initialization

--- Snake-Game.js
+++ Snake-Game.js
@@ -1,16 +1,37 @@
+const ROW_SIZE = 20;
+const COLUMN_SIZE = 20;
+const PIXEL_WIDTH = 20;
+var gameboard;
+var snake = [[0,0],[0,1],[0,2],[0,3]];
 initialize();
 
-function initialize(){
-  for (let i = 1; i<=400; i++){
-    var div = document.createElement('div');
-    var gameboardDiv = document.getElementById("gameboard");
-    gameboardDiv.appendChild(div);
-    div.className = 'pixel';
-    
-    for (let x = 0; x<260 ; x++)
-    div.style.top = x;
-    for (let y = 0; y<169; y++)
-    div.style.left = y
 
+function initializeGameBoard(){
+  gameboard = new Array(ROW_SIZE);
+  for(let row =0; row<ROW_SIZE; row++){
+    gameboard[row] = new Array(COLUMN_SIZE);
+    for(let column = 0; column<COLUMN_SIZE; column++){
+      let div = document.createElement('div');
+      let gameboardDiv = document.getElementById("gameboard");
+      gameboardDiv.appendChild(div);
+      div.className = "pixel";
+      div.style.left = column *PIXEL_WIDTH;
+      div.style.top = row *PIXEL_WIDTH;
+      gameboard[row][column] = div;
+    }
   }
 }
+
+function initializeSnake(){
+  gameboard[0][0].style.backgroundColor = "red";
+  gameboard[0][1].style.backgroundColor = "blue";
+  gameboard[0][2].style.backgroundColor = "blue";
+  gameboard[0][3].style.backgroundColor = "blue";
+}
+
+function initialize(){
+  initializeGameBoard();
+  initializeSnake();
+  initializeFood();
+}
+
