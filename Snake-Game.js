8a9511458c422e8be42308ae947e3100cb7fbad9
Valderhide <gpinsent@hotmail.com>
2021-08-30 13:16:49 -0230

    Snake Growth and Food reposition working

--- Snake-Game.js
+++ Snake-Game.js
@@ -2,56 +2,79 @@ const ROW_SIZE = 20;
 const COLUMN_SIZE = 20;
 const PIXEL_WIDTH = 20;
 var gameboard;
-var snake;
-let headX = Math.floor(ROW_SIZE / 2);  
+let snake;
+let food;
+let headX = Math.floor(ROW_SIZE / 2);
 let headY = Math.floor(COLUMN_SIZE / 2);
 initialize();
 
-function initialize(){
+function initialize() {
   initializeGameBoard();
   initializeSnake();
   initializeFood();
+  drawBoard();
 }
 
 
-
-
-    function initializeGameBoard(){
+function initializeGameBoard() {
   gameboard = new Array(ROW_SIZE);
-  for(let row =0; row<ROW_SIZE; row++){
+  for (let row = 0; row < ROW_SIZE; row++) {
     gameboard[row] = new Array(COLUMN_SIZE);
-    for(let column = 0; column<COLUMN_SIZE; column++){
+    for (let column = 0; column < COLUMN_SIZE; column++) {
       let div = document.createElement('div');
       let gameboardDiv = document.getElementById("gameboard");
       gameboardDiv.appendChild(div);
       div.className = "pixel";
-      div.style.left = column *PIXEL_WIDTH;
-      div.style.top = row *PIXEL_WIDTH;
+      div.style.left = column * PIXEL_WIDTH;
+      div.style.top = row * PIXEL_WIDTH;
       gameboard[row][column] = div;
-      }
     }
-
   }
-
-
-function initializeSnake(){
-  moveSnake([[headX,headY],[headX,headY+1],[headX,headY+2],[headX,headY+3]]);
 }
 
-function initializeFood(){
+function drawBoard() {
+  initializeGameBoard();
+  drawFood(food);
+  drawSnake(snake);
+  snakeGrowth();
+}
+
+function snakeGrowth(){
+  if (gameboard[food.x][food.y] == gameboard[snake[0].x][snake[0].y]) {
+  snake.push(snake);
+  initializeFood();
+  }
+}
+
+function drawFood(food) {
+  gameboard[food.x][food.y].style.backgroundColor = "black";
+}
+
+function drawSnake(snake) {
+  for (position of snake) {
+    gameboard[position.x][position.y].style.backgroundColor = "blue";
+  }
+  gameboard[snake[0].x][snake[0].y].style.backgroundColor = "red";
+}
+
+function initializeSnake() {
+  snake =[{ x: headX, y: headY }, { x: headX, y: headY + 1 }, { x: headX, y: headY + 2 }, { x: headX, y: headY + 3 }];
+}
+
+function initializeFood() {
   let x, y;
-  do{
+  do {
     x = Math.floor(Math.random() * ROW_SIZE);
     y = Math.floor(Math.random() * COLUMN_SIZE);
   }
-  while(snakeIncludes([x,y]));
+  while (snakeIncludes([x, y]));
 
-  gameboard[x][y].style.backgroundColor = "black";
+  food = { x, y };
 }
 
-function snakeIncludes(position){
-  for (let i = 0; i < snake.length; i++ ){
-    if(snake[i][0] == position[0] && snake[i][1] == position[1]){
+function snakeIncludes(position) {
+  for (let i = 0; i < snake.length; i++) {
+    if (snake[i][0] == position[0] && snake[i][1] == position[1]) {
       return true;
     }
   }
@@ -59,46 +82,33 @@ function snakeIncludes(position){
   return false;
 }
 
+function moveSnake(newSnakePosition) {
+  snake.unshift(newSnakePosition);
+  snake.pop();
+}
 
-
-function moveSnake(newSnakePosition){
-  //#B6D7A8 = grid color
-  for (let i = 0; i < newSnakePosition.length; i++ ){
-    let pixelColor;
-    if (i==0) {
-      pixelColor = "red";
-    }
-    else {
-      pixelColor = "blue";
-    }
-    gameboard[headX][headY].style.backgroundColor = 'white'
-    gameboard[newSnakePosition[i][0]][newSnakePosition[i][1]].style.backgroundColor = pixelColor;
-    snake = newSnakePosition;
+document.onkeydown = function (e) {
+  switch (e.key) {
+    case 'ArrowUp':
+      headX--;
+      moveSnake({ x: headX, y: headY });
+      drawBoard();
+      break;
+    case 'ArrowDown':
+      headX++;
+      moveSnake({ x: headX, y: headY });
+      drawBoard();
+      break;
+    case 'ArrowLeft':
+      headY--;
+      moveSnake({ x: headX, y: headY });
+      drawBoard();
+      break;
+    case 'ArrowRight':
+      headY++
+      moveSnake({ x: headX, y: headY });
+      drawBoard();
+      break;
   }
 
-  function update() {
-    for (let i = newSnakePosition.length - 3; i >= 0; i--){
-      newSnakePosition[i+1] = {...newSnakePosition[i]}
-    }
-  }
-
-  document.onkeydown = function(e) {
-    switch (e.key) {
-        case 'ArrowUp':
-          moveSnake([[headX--,headY]]);
-          update();
-            break;
-        case 'ArrowDown':
-          moveSnake([[headX++,headY]]);
-            break;
-        case 'ArrowLeft':
-          moveSnake([[headX,headY--]]);
-            break;
-        case 'ArrowRight':
-          moveSnake([[headX,headY++]]);
-            break;
-
-
-     }
-  }
 }
